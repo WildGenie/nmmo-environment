30b1043185f8a1b78899af50d01d00a2a5014559
Muhammad <umair.nasir014@gmail.com>
2022-11-08 05:46:05 +0200

    Added structure for core/relations.Relations class

---
+++ nmmo/core/relations.py
@@ -0,0 +1,19 @@
+
+
+class Relations:
+
+    def __init__(self,left,right,all,*args) -> None:
+        pass
+
+    def atomic_tasks(self) -> None:
+        pass
+    
+    def to_string(self) -> None:
+        pass
+
+    def short_circuit_mech(self) -> None:
+        pass
+
+    @staticmethod
+    def sample_relation(self) -> None:
+        pass
