7be58ace93cdae28a400984690e89bac573d9432
Muhammad <umair.nasir014@gmail.com>
2022-11-12 21:51:35 +0200

    some atomic tasks defined

--- nmmo/core/relations.py
+++ nmmo/core/relations.py
@@ -1,5 +1,51 @@
 import random
 
+from nmmo import Task
+
+"""
+
+Copied from nmmo-baselines.tasks
+
+"""
+
+class Tier:
+    REWARD_SCALE = 15
+    EASY         = 4  / REWARD_SCALE
+    NORMAL       = 6  / REWARD_SCALE
+    HARD         = 11 / REWARD_SCALE
+
+def player_kills(realm, player):
+    return player.history.playerKills
+
+def equipment(realm, player):
+    return player.equipment.total(lambda e: e.level)
+
+def exploration(realm, player):
+    return player.history.exploration
+
+def foraging(realm, player):
+    return max(
+            player.skills.fishing.level.val,
+            player.skills.herbalism.level.val,
+            player.skills.prospecting.level.val,
+            player.skills.carving.level.val,
+            player.skills.alchemy.level.val)
+
+def combat(realm, player):
+    return max(
+            player.skills.mage.level.val,
+            player.skills.range.level.val,
+            player.skills.melee.level.val)
+
+
+"""
+
+End of the copied section
+
+"""
+
+RELATIONS = [player_kills, equipment, exploration, foraging, combat]
+
 class Relations:
 
     def __init__(self,teams):
@@ -19,10 +65,43 @@ class Relations:
             raise ValueError(f"{self.teams} is invalid argument")
 
 
+        self.relations = []
+
+
     def atomic_tasks(self):
-        pass
+        
+        relation = self.sample_relation(RELATIONS)
+
+        assert len(relation) == 1
+
+        if relation == player_kills:
+            return [Task(player_kills, 1, Tier.EASY),
+                    Task(player_kills, 2, Tier.NORMAL),
+                    Task(player_kills, 3, Tier.HARD)]
+        
+        elif relation == equipment:
+            return [Task(equipment, 1,  Tier.EASY),
+                    Task(equipment, 5, Tier.NORMAL),
+                    Task(equipment, 10, Tier.HARD)]
+
+        elif relation == exploration:
+            return [Task(exploration, 16,  Tier.EASY),
+                    Task(exploration, 32,  Tier.NORMAL),
+                    Task(exploration, 64, Tier.HARD)]
+
+        elif relation == foraging:
+            return [Task(foraging, 2, Tier.EASY),
+                    Task(foraging, 3, Tier.NORMAL),
+                    Task(foraging, 4, Tier.HARD)]
+
+        elif relation == combat:
+            return [Task(combat, 2, Tier.EASY),
+                    Task(combat, 3, Tier.NORMAL),
+                    Task(combat, 4, Tier.HARD)]
+
+
     
-    def to_string(self):
+    def natural_language_to_string(self):
         pass
 
     def short_circuit_mech(self):
@@ -30,5 +109,9 @@ class Relations:
 
     @staticmethod
     def sample_relation(relations):
-        relations_keys = list(relations.keys())
-        return random.sample(len(relations_keys),k=1)
+        return random.sample(len(relations),k=1)
+
+
+
+
+
