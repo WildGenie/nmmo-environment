b2c4fdb02989e754c1c60525e46806abc88cf96c
Muhammad <umair.nasir014@gmail.com>
2022-11-12 19:30:22 +0200

    Started with sample_relations

--- nmmo/core/relations.py
+++ nmmo/core/relations.py
@@ -1,19 +1,34 @@
-
+import random
 
 class Relations:
 
-    def __init__(self,left,right,all,*args) -> None:
-        pass
+    def __init__(self,teams):
+        
+        self.teams = teams
 
-    def atomic_tasks(self) -> None:
+        if self.teams == 'all':
+            pass
+        
+        elif self.teams == 'left':
+            pass
+
+        elif self.teams == 'right':
+            pass
+
+        else:
+            raise ValueError(f"{self.teams} is invalid argument")
+
+
+    def atomic_tasks(self):
         pass
     
-    def to_string(self) -> None:
+    def to_string(self):
         pass
 
-    def short_circuit_mech(self) -> None:
+    def short_circuit_mech(self):
         pass
 
     @staticmethod
-    def sample_relation(self) -> None:
-        pass
+    def sample_relation(relations):
+        relations_keys = list(relations.keys())
+        return random.sample(len(relations_keys),k=1)
