b15133cfc5b86be96f02ae32fec8cb172650ca22
Muhammad <umair.nasir014@gmail.com>
2022-11-15 22:25:05 +0200

    Updated Relations

--- nmmo/core/relations.py
+++ nmmo/core/relations.py
@@ -1,122 +1,66 @@
 import random
 
-from nmmo import Task
-
-"""
-
-Copied from nmmo-baselines.tasks
-
-"""
-
-class Tier:
-    REWARD_SCALE = 15
-    EASY         = 4  / REWARD_SCALE
-    NORMAL       = 6  / REWARD_SCALE
-    HARD         = 11 / REWARD_SCALE
-
-def player_kills(realm, player):
-    return player.history.playerKills
-
-def equipment(realm, player):
-    return player.equipment.total(lambda e: e.level)
-
-def exploration(realm, player):
-    return player.history.exploration
-
-def foraging(realm, player):
-    return max(
-            player.skills.fishing.level.val,
-            player.skills.herbalism.level.val,
-            player.skills.prospecting.level.val,
-            player.skills.carving.level.val,
-            player.skills.alchemy.level.val)
-
-def combat(realm, player):
-    return max(
-            player.skills.mage.level.val,
-            player.skills.range.level.val,
-            player.skills.melee.level.val)
 
 
-"""
 
-End of the copied section
+class Task:
+    def __init__(self, *tasks):
+        self.tasks = tasks
 
-"""
+    def to_natural_language(self):
+        return ' '.join([e.to_natural_language() for e in self.tasks])
 
-RELATIONS = [player_kills, equipment, exploration, foraging, combat]
+class Relation:
+    def __init__(self, team):
+        self.team = team
 
-
-class Relations:
-
-    def __init__(self,teams):
-        
-        self.teams = teams
-
-        if self.teams == 'all':
-            pass
-        
-        elif self.teams == 'left':
-            pass
-
-        elif self.teams == 'right':
-            pass
-
-        else:
-            raise ValueError(f"{self.teams} is invalid argument")
-
-    def atomic_tasks(self):
-        pass
-    
-    def natural_language_to_string(self):
+    def to_natural_language(self):
         pass
 
     def short_circuit_mech(self):
         pass
 
+    @property
+    def param_space(self):
+        pass
+
     @staticmethod
-    def sample_relation(relations):
-        return random.sample(len(relations),k=1)
+    def sample(self):
+        return random.sample(self.param_space,k=1)
 
-class Hoarding(Relations):
+class Equipment(Relation):
 
-    def __init__(self,teams):
+    def __init__(self, teams, level):
         super().__init__(teams)
-        
+        assert level in self.param_space
+        self.level = level
 
-    def atomic_tasks(self):
-        
-        relation = self.sample_relation(RELATIONS)
+    @property
+    def param_space(self):
+        return [1, 5, 10]
 
-        assert len(relation) == 1
-
-        
-        if relation == equipment:
-            return [Task(equipment, 1,  Tier.EASY),
-                    Task(equipment, 5, Tier.NORMAL),
-                    Task(equipment, 10, Tier.HARD)]
-
-        elif relation == exploration:
-            return [Task(exploration, 16,  Tier.EASY),
-                    Task(exploration, 32,  Tier.NORMAL),
-                    Task(exploration, 64, Tier.HARD)]
-
-        elif relation == foraging:
-            return [Task(foraging, 2, Tier.EASY),
-                    Task(foraging, 3, Tier.NORMAL),
-                    Task(foraging, 4, Tier.HARD)]
+    def completed(self, realm):
+        return self.team.equipment.total(lambda e: e.level) > self.level
 
 
-    
-    def natural_language_to_string(self):
-        pass
+class TeamKills(Relation):
 
-    def short_circuit_mech(self):
-        pass
+    def __init__(self, teams, level):
+        super().__init__(teams)
+        assert level in self.param_space
+        self.level = level
 
-    @staticmethod
-    def sample_relation(relations):
-        return random.sample(len(relations),k=1)
+    @property
+    def param_space(self):
+        return [1, 4, 8]
+
+    def completed(self, realm):
+        return self.team.history.playerKills > self.level
+
+
+
+atomic_equipment_task = Task(Equipment(5))
+atomic_teamkill_task = Task(TeamKills(4))
 
 
 
